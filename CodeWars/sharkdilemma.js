d81e7b24733536e41f60a170a64e3f607278a7de
Andrew Griffin <62045638+gryphonsoccer@users.noreply.github.com>
2022-05-13 23:39:50 -0500

    Function fully updated
    no bugs in code, final version

--- CodeWars/sharkdilemma.js
+++ CodeWars/sharkdilemma.js
@@ -1,3 +1,22 @@
 function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
-  
+        pontoonDistance = pontoonDistance
+        if(dolphin){
+            sharkSpeed = sharkSpeed/2
+        }else{
+            sharkSpeed = sharkSpeed
+        }
+        while (pontoonDistance > 0 || sharkDistance>0){
+            console.log(`you are ${pontoonDistance} meters away`);
+            console.log(`the shark is ${sharkDistance} meters away`);
+            sharkDistance -= sharkSpeed;
+            pontoonDistance -= youSpeed;
+            if(pontoonDistance<=0){
+               return 'Alive';     
+            }else if(sharkDistance<=0){
+                return 'Shark Bait!'
+            }
+
+
+        }
 }
+console.log(shark(24, 0, 4, 8, true))
