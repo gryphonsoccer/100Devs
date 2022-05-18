d62ed8d8ddd23414f104aedd9519c67df4ed96e5
Andrew Griffin <62045638+gryphonsoccer@users.noreply.github.com>
2022-05-17 23:53:57 -0500

    2nd commit to treat cancer
    2nd commit to treat cancer

---
+++ CodeWars/treatcancer.js
@@ -0,0 +1,12 @@
+function cutCancerCells(organism){
+    bob = organism.split('')
+    joe = bob.filter(item => item !=='c')
+    for(i=0;i>joe.length;i++){
+        if(joe[i]==='C'){
+            
+        }
+    }
+    return joe.join('')
+  }
+
+ console.log( cutCancerCells('ccboobscc'))
